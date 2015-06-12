10b209bd5962b97b89762ffb15c9102d22c00425
ofirENS <oshukron@biologie.ens.fr>
2015-06-12 15:25:41 +0200

    added  time for fix, in which the bending elasticity and the spring force are equal. At beam time the diffusion is stoped the affected beads are mainly affected by bending, spring force is decreased. after fix time the spring force restore to original vaues

--- Code/scrTestDNADensityInROI.m
+++ Code/scrTestDNADensityInROI.m
@@ -4,42 +4,46 @@
 % indipendent code
 % parameter values are set proportional to (dimension*diffusionConst/b^2)
 close all 
-numParticles     = 800;
+%% parameters
+numParticles     = 600;
 dimension        = 3;
 % relxation time 
 % (numParticles*b)^2 / (3*diffusionConst*pi^2)
-numSteps         = 400;
+numSteps         = 800;
 beamTime         = 200; 
-dt               = 0.01;
+fixTime          = 600;
+dt               = 0.001;
 angle0           = pi;
 b                = sqrt(3);
 diffusionConst   = 1;
-springConst      = 2*dimension*diffusionConst./b^2;
+springConst      = 2*(dimension*diffusionConst./b^2)*ones(numParticles);
 bendingConst     = 1*dimension*diffusionConst./b^2;
 particlePosition = cumsum(sqrt(2*diffusionConst*dt)*randn(numParticles,dimension));
 
 connectivityMap  = (diag(ones(1,numParticles-1),1)+diag(ones(1,numParticles-1),-1))~=0;
-minParticleDist  = 1;
+minParticleDist  = 0;
 fixedParticleNum = [];
 
-gyrationRadius      = sqrt(numParticles/6)*b; % for large numParticles
-beamRad             = gyrationRadius/8;
-roiRadius           = gyrationRadius/5;
-affectedBeads       = false(numParticles,1);
+gyrationRadius   = sqrt(numParticles/6)*b; % for large numParticles
+beamRad          = gyrationRadius/15;
+roiRadius        = gyrationRadius/10;
+roiRes           = 20; % number of rows and columns in roi
+affectedBeads    = false(numParticles,1);
 
 % flags
-diffusionFlag      = true;
-springsFlag        = true;
-bendingFlag        = true;
+diffusionFlag    = true;
+springsFlag      = true;
+bendingFlag      = true;
 
+%% Create Graphics
 
 % Create graphics
 mainFig       = figure('Units','norm');
 densityFigure = figure('Units','norm');
 projFig       = figure('Units','norm');  
-mainAxes      = axes('Parent',mainFig,'Units','norm','Color','k','XLim',5*gyrationRadius.*[-1 1],...
-                     'YLim',5*gyrationRadius.*[-1 1],'ZLim',5*gyrationRadius.*[-1 1],'FontSize',25);
-densityAxes   = axes('Parent',densityFigure,'Units','norm','FontSize',25,'YLim',[0 numParticles]);
+mainAxes      = axes('Parent',mainFig,'Units','norm','Color','k','XLim',gyrationRadius.*[-1 1],...
+                     'YLim',gyrationRadius.*[-1 1],'ZLim',gyrationRadius.*[-1 1],'FontSize',25);
+densityAxes   = axes('Parent',densityFigure,'Units','norm','FontSize',25,'YLim',[0 1]);
 projAxes      = axes('Parent',projFig,'Units','norm','FontSize',25,'XLim',get(mainAxes,'XLim'),'YLim',get(mainAxes,'YLim'));
 
 xlabel(densityAxes,'Time'); ylabel(densityAxes,'Num. Beads In ROI')
@@ -92,13 +96,17 @@ roiHandle           = patch([cm(1)-roiRadius,cm(1)+roiRadius,cm(1)+roiRadius,cm(
                                 'Parent',mainAxes,'FaceAlpha',0.5);
 projRoiHandle       = patch([cm(1)-roiRadius,cm(1)+roiRadius,cm(1)+roiRadius,cm(1)-roiRadius],...
                             [cm(2)-roiRadius,cm(2)-roiRadius,cm(2)+roiRadius,cm(2)+roiRadius],'y',...
-                                'Parent',projAxes,'FaceAlpha',0.5);   
+                                'Parent',projAxes,'FaceAlpha',0.5);  
+                            
+baseLineDensity = ConcentricDensityInRoi(particlePosition,[cm(1)-roiRadius,cm(2)-roiRadius,2*roiRadius,2*roiRadius],roiRes);
+
+%% Run Simulation                            
 for sIdx = 1:numSteps
     
     particleDist = ForceManager.GetParticleDistance(particlePosition);
     
     % Calculate forces
-    bendingForce   = ForceManager.GetBendingElasticityForce(bendingFlag,particlePosition,particleDist,[],bendingConst,[],angle0);
+    bendingForce   = ForceManager.GetBendingElasticityForce(bendingFlag,particlePosition,particleDist,connectivityMap,bendingConst,fixedParticleNum,angle0);
     bendingForce(~affectedBeads,:) = 0;% zero out bending forces for unaffected beads 
     
     diffusionForce = ForceManager.GetDiffusionForce(diffusionFlag,particlePosition,diffusionConst,dt,fixedParticleNum);
@@ -107,9 +115,12 @@ for sIdx = 1:numSteps
     % Update position
     particlePosition = particlePosition +(bendingFlag*bendingForce +springsFlag*springForce)*dt+ diffusionFlag*diffusionForce;
     
-    cm    = mean(particlePosition,1); % center of mass
-    inROI = (particlePosition(:,1)<(cm(1)+roiRadius)) & (particlePosition(:,1)>(cm(1)-roiRadius)) & ...
-            (particlePosition(:,2)<(cm(2)+roiRadius)) & (particlePosition(:,2)>(cm(2)-roiRadius));
+    cm               = mean(particlePosition,1); % center of mass
+    
+    density = ConcentricDensityInRoi(particlePosition,[cm(1)-roiRadius,cm(2)-roiRadius,2*roiRadius,2*roiRadius],roiRes);
+    
+%     inROI = (particlePosition(:,1)<(cm(1)+roiRadius)) & (particlePosition(:,1)>(cm(1)-roiRadius)) & ...
+%             (particlePosition(:,2)<(cm(2)+roiRadius)) & (particlePosition(:,2)>(cm(2)-roiRadius));
             
     % Update graphics
     set(particleHandle,'XData',particlePosition(:,1),...
@@ -123,8 +134,9 @@ for sIdx = 1:numSteps
     set(affectedBeadsHandle,'XData', particlePosition(affectedBeads,1),...
                             'YData',particlePosition(affectedBeads,2),...
                             'ZData',particlePosition(affectedBeads,3));
-    set(numBeadsInHandle,'XData',[get(numBeadsInHandle,'XData'),sIdx*dt],...
-                         'YData',[get(numBeadsInHandle,'YData'), sum(inROI)])
+    set(numBeadsInHandle,'XData',(0:(roiRes-2)),...
+                         'YData',(density./baseLineDensity(1)))
+                     
     set([roiHandle,projRoiHandle],'XData',[cm(1)-roiRadius,cm(1)+roiRadius,cm(1)+roiRadius,cm(1)-roiRadius],...
                   'YData',[cm(2)-roiRadius,cm(2)-roiRadius,cm(2)+roiRadius,cm(2)+roiRadius]);
               
@@ -137,7 +149,7 @@ for sIdx = 1:numSteps
     if sIdx ==beamTime
         % Apply beam on particles
         % find all particles around center of mass in radius beamRad
-        affectedBeads       = sqrt(sum(bsxfun(@minus,particlePosition(:,1:2),cm(1:2)).^2,2))<=beamRad;
+        affectedBeads       = sum(bsxfun(@minus,particlePosition(:,1:2),cm(1:2)).^2,2)<=beamRad^2;
         affectedBeadsHandle = line('XData',particlePosition(affectedBeads ,1),...
                                    'Ydata',particlePosition(affectedBeads ,2),...
                                    'Zdata',particlePosition(affectedBeads ,3),...
@@ -146,11 +158,34 @@ for sIdx = 1:numSteps
         for cIdx = 1:numCirclesInBeam
             set(beamCircleHandle(cIdx),'Visible','on')
         end
-        set(affectedBeadsProjHandle,'Visible','on'); 
+        
+        set(affectedBeadsProjHandle,'Visible','on','XData',particlePosition(affectedBeads,1),...                         
+                                    'YData',particlePosition(affectedBeads,2));
+        
+        % calculate baseLine density
+        baseLineDensity = ConcentricDensityInRoi(particlePosition,[cm(1)-roiRadius,cm(2)-roiRadius,2*roiRadius,2*roiRadius],roiRes);
+        
         % turn off diffusion 
         diffusionFlag = false;
+        for aIdx = 1:numParticles            
+            if affectedBeads(aIdx)
+               springConst(aIdx,aIdx) = springConst(aIdx,aIdx)/2;
+            end
+        end
         sprintf('%s%f','Beam fired at time: ', sIdx*dt)
+        
+    end
+    
+    if sIdx ==fixTime
+        % fixind time 
+      for aIdx = 1:numParticles            
+            if affectedBeads(aIdx)
+               springConst(aIdx,aIdx) = springConst(aIdx,aIdx)*2;
+            end
+      end
+        affectedBeads = false(numParticles,1);
+        sprintf('%sf','Fixed at time: ', sIdx*dt); 
     end
 end
-
+%% save Results
 save particlePosition particlePosition

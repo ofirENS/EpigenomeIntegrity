3f956600ecf49f3694ce3cd35486e660f4228185
ofirENS <oshukron@biologie.ens.fr>
2015-06-12 10:30:29 +0200

    changes to the code

--- Code/BeamDamageParams.m
+++ Code/BeamDamageParams.m
@@ -1,15 +1,39 @@
 classdef BeamDamageParams<handle %{UNFINISHED}
     
     properties
+        numParticles@double        
         numRelaxationSteps@double
         numRecordingSteps@double
         numBeamSteps@double
-        roiParams =struct('rectX',[],'rectY',[],'rectWidth',[],'rectHeight');
+        dimension@double
+        dt@double
+        connectivityMat@logical
+        forceParams = ForceManagerParams;
+        roiRadius@double
+        beamRad@double
+        gyrationRadius@double
+        particlePosition@double 
+        loadConfiguration@logical
+        saveAfterRelaxationTime@logical
+        saveAfterBeamTime@logical
+        showSimulation@logical
+% relxation time 
+% (numParticles*b)^2 / (3*diffusionConst*pi^2)
     end
     
     methods         
         function obj = BeamDamageParams(varargin)
-
+             obj.SetInputParams(vrargin)
+        end
+        
+        function SetInputParams(obj,varargin)
+            if mod(numel(varargin),2)~=0
+                error(' input argument must come in name value pairs')
+            else
+                for vIdx = 1:numel(varargin)
+                    obj.(varargin{2*vIdx-1}) = varargin{2*vIdx};
+                end
+            end
         end
     end
     

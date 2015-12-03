6632fab0114cd4c5257c033e19af565c5264ef13
ofirEns <ofirshukron@gmail.com>
2015-12-03 20:37:24 +0100

    equations modified to include N_open and N_slide

--- Code/scripts/scrFiguresModelFitDoseDependent.m
+++ Code/scripts/scrFiguresModelFitDoseDependent.m
@@ -16,7 +16,7 @@ showSlidingFraction = true;
 showSlidingOutOfDR  = true;
 showRelativeSliding = true;
 showExpansionFactor = true;
-showRelativeOpeningDNA  = true;
+showRelativeOpeningDNA      = true;
 showRelativeOpeningHistone  = true;
 
 % Experimental measurements 
@@ -25,22 +25,25 @@ showRelativeOpeningHistone  = true;
 uData = [0 5 10	15	20	25	30	35	40	45	50	55	60	65	70	75 100];
 %__ histone loss data___
 hData = [0 10.714305725	10.8220788165	14.4014983755	20.8225447327	21.2024074872	21.3668579387	29.195045218	37.2706560079	37.3479226024	42.5138151765	42.9133041668	42.8508770934	43.8660779761	42.5763929893	44.1947934168 	40.8353794651
-]./100;
+];
 
 %___DNA loss data____
 dData = [0 1.5704212005	1.1365167475	4.545552178	8.7406190878	9.8581219326	10.2900341153	12.6333239455	20.0360763966	22.3129622161	22.5107680397	22.7887958612	20.4006799168	21.1679155925	22.757261652	26.9902966182	26.4974599239
-]./100;
+];
 
 % Analytical solutions of the model for histones and DNA loss vs UV dose
-T = @(a1,u)  (1-exp(-a1.*u));% T(u)/T_max
+T = @(a1,u)  (1-exp(-a1.*u)).^2;% T(u)/T_max
 % T = @(a1,u) ((1-exp(-(a1.^2).*u))+sqrt(pi.*u).*a1.*(1-erf(a1.*sqrt(u)))).^2; 
 % %-- quadratic (damages) system 
 % T  = @(a1,u) a1.*u.^2;
 
-N = @(a1,a2,u)exp(-(a2).*T(a1,u)); % N(u)/N_0
-R = @(a1,a2,a3,a4,u) (1+a3.*(1-N(a1,a2,u)).*T(a1,u)+(a4).*(T(a1,u))); %R(u)/R_0
-d = @(a1,a2,a3,a4,u) ((R(a1,a2,a3,a4,u))-T(a1,u))./(R(a1,a2,a3,a4,u));
-h = @(a1,a2,a3,a4,u) d(a1,a2,a3,a4,u)+T(a1,u).*(1-N(a1,a2,u))./R(a1,a2,a3,a4,u);%d(a1,a2,a3,a4,u)+(T(a1,u)-N(a1,a2,u))./R(a1,a2,a3,a4,u) ;%./R(a1,a2,a3,u);
+N_open  = @(a1,a2,a3,u)a3.*exp(-a2.*(1-a3).*T(a1,u)); % out due to pushing
+N_slide = @(a1,a2,a3,u)(1-exp(-a2.*(1-a3).*T(a1,u))); % out due to sliding
+N_total = @(a1,a2,a3,u) N_open(a1,a2,a3,u)+N_slide(a1,a2,a3,u); % total out of IDR
+% N = @(a1,a2,u)exp(-(a2).*T(a1,u))+(1-T(a1,u)); % N(u)/N_0
+R = @(a1,a2,a3,a4,u)  1+a4.*N_total(a1,a2,a3,u);%+(a4).*(T(a1,u))); %R(u)/R_0
+d = @(a1,a2,a3,a4,u) 100.*(((R(a1,a2,a3,a4,u))-1+N_open(a1,a2,a3,u))./(R(a1,a2,a3,a4,u)));%+(T(a1,u))./R(a1,a2,a3,a4,u);
+h = @(a1,a2,a3,a4,u) 100*(d(a1,a2,a3,a4,u)./100 +(N_slide(a1,a2,a3,u))./R(a1,a2,a3,a4,u));%d(a1,a2,a3,a4,u)+(T(a1,u)-N(a1,a2,u))./R(a1,a2,a3,a4,u) ;%./R(a1,a2,a3,u);
 
 % %--- full (damages) system
 % c1 = 0.0021;%0.021;% curve h
@@ -65,26 +68,27 @@ h = @(a1,a2,a3,a4,u) d(a1,a2,a3,a4,u)+T(a1,u).*(1-N(a1,a2,u))./R(a1,a2,a3,a4,u);
 % c4 = 0.2; %0.22; % lift h and d
 
 % % ---autofit
-opt       = optimset('TolFun',1e-15,'TolX',1e-15,'MaxIter',1e6,'MaxFunEvals',1e6,'TolCon',1e-18,'Hessian','bfgs');
-% run several tests
-numTests  = 4;
-fitParams = zeros(numTests,4); 
-fval      = zeros(numTests,1);
-for tIdx = 1:numTests
-[fitParams(tIdx,:),fval(tIdx),exitFlag,output]=...
-    fmincon(@FitDandH,1*rand(1,4),-1*eye(4),zeros(4,1),[],[],zeros(4,1),5*ones(4,1),[],opt);
-end
-[~,pl] = min(fval);
- fitParams = fitParams(pl,:);
-c1 = fitParams(1);
-c2 = fitParams(2);
-c3 = fitParams(3);
-c4 = fitParams(4);
+% opt       = optimset('TolFun',1e-15,'TolX',1e-15,'MaxIter',1e7,'MaxFunEvals',1e7,'TolCon',1e-19,'Hessian','bfgs',...
+%     'Diagnostics','off');
+% % run several tests
+% numTests  = 1;
+% fitParams = zeros(numTests,4); 
+% fval      = zeros(numTests,1);
+% for tIdx = 1:numTests
+% [fitParams(tIdx,:),fval(tIdx),exitFlag,output]=...
+%     fmincon(@FitDandH,2*rand(1,4),-1*eye(4),zeros(4,1),[],[],zeros(4,1),200*ones(4,1),[],opt);
+% end
+% [~,pl] = min(fval);
+%  fitParams = fitParams(pl,:);
+% c1 = fitParams(1);
+% c2 = fitParams(2);
+% c3 = fitParams(3);
+% c4 = fitParams(4);
 
-% c1 = 0.03;%0.021;% curve h
-% c2 = 0.01;  %0.39;% lift h
-% c3 = 0.1; %0.44; % lift d+h
-% c4 = 0.1; %0.23; % lift d+ h
+c1 = 0.0347;%0.021;% curve h
+c2 = 0.3858;  %0.39;% lift h
+c3 = 0.01; %0.44; % lift d+h
+c4 = 1.5; %0.23; % lift d+ h
 
 %-- plot ---
 if showHAndDFit
@@ -129,10 +133,10 @@ if showSlidingOutOfDR
 %_____ fraction of sliding out of the damage region (h-d)/(1-d)
 fig3 = figure; 
 ax3  = axes('Parent',fig3,'NextPlot','add');
-line('XData',uData,'Ydata',(hData-dData)./(1-dData),'Marker','o','Color','k','MarkerFaceColor','c',...
+line('XData',uData,'Ydata',(hData-dData)./(100-dData),'Marker','o','Color','k','MarkerFaceColor','c',...
     'Parent',ax3,'DisplayName','histone sliding out of DR, exp. data','MarkerSize',markerSize,...
     'LineStyle','none');
-line('XData',uData,'YData',(h(c1,c2,c3,c4,uData)-d(c1,c2,c3,c4,uData))./(1-d(c1,c2,c3,c4,uData)),'Parent',ax3,...
+line('XData',uData,'YData',(h(c1,c2,c3,c4,uData)-d(c1,c2,c3,c4,uData))./(100-d(c1,c2,c3,c4,uData)),'Parent',ax3,...
     'LineWidth',lineWidth,'DisplayName','histone sliding out of DR, model')
 % add linear fit for comparison
 % linearFitModel = fittype('a*x');
@@ -154,7 +158,7 @@ ax4  = axes('Parent',fig4,'NextPlot','add','FontSize',fontSize);
 line('XData',uData,'Ydata',1 -(dData)./hData,'Marker','o','Color','k','MarkerFaceColor','c',...
     'Parent',ax4,'DisplayName','relative histone sliding contribution, exp. data','MarkerSize',markerSize,...
     'LineStyle','none');
-line('XData',uData,'YData',1-(d(c1,c2,c3,c4,uData))./(h(c1,c2,c3,c4,uData)),'Parent',ax4,...
+line('XData',uData,'YData',((h(c1,c2,c3,c4,uData))-d(c1,c2,c3,c4,uData))./(h(c1,c2,c3,c4,uData)),'Parent',ax4,...
     'LineWidth',lineWidth,'DisplayName','relative histone sliding contribution, model')
 xlabel('U.V dose','Parent',ax4,'FontSize',fontSize);
 ylabel('(h(U)-d(U))/h(U)','FontSize',fontSize,'Parent',ax4)
@@ -184,9 +188,13 @@ ax6 = axes('Parent', fig6,'NextPlot','Add','FontSize',fontSize,'LineWidth',lineW
 title('Relative contribution to DNA loss','FontSize',fontSize,'Parent',ax6)
 xlabel(ax6,'UV dose','FontSize',fontSize)
 ylabel(ax6,'Fraction of loss','FontSize',fontSize)
-relativeOpening = [(c4.*T(c1,uVals))./(c3.*(1-N(c1,c2,uVals))+c4.*T(c1,uVals));...
-    1-(c4.*T(c1,uVals))./(c3.*(1-N(c1,c2,uVals))+c4.*T(c1,uVals))]';
-bar(uVals,relativeOpening,1,'Stacked')
+d_open = 100*(R(c1,c2,c3,c4,uVals)+N_open(c1,c2,c3,uVals)-c4*N_slide(c1,c2,c3,uVals)-1)./(R(c1,c2,c3,c4,uVals)+N_open(c1,c2,c3,uVals)-c4*N_slide(c1,c2,c3,uVals));
+
+relativeOpening = [d_open./d(c1,c2,c3,c4,uVals);
+                   1-d_open./d(c1,c2,c3,c4,uVals)];
+% relativeOpening = [(c4.*T(c1,uVals))./(c3.*(1-N(c1,c2,uVals))+c4.*T(c1,uVals));...
+%     1-(c4.*T(c1,uVals))./(c3.*(1-N(c1,c2,uVals))+c4.*T(c1,uVals))]';
+bar(uVals,relativeOpening',1,'Stacked')
 c = get(ax6,'Children');
 set(c(1),'LineStyle','none','FaceColor','y');
 set(c(2),'LineStyle','none')
@@ -214,8 +222,9 @@ if showRelativeOpeningHistone
   fig7 = figure;
   uVals = 0:0.5:max(uData);
   ax7 = axes('Parent',fig7,'NextPlot','Add');
-  relativeOpeningH = [c4.*T(c1,uVals)./(R(c1,c2,c3,c4,uVals)-N(c1,c2,uVals));...(1-(1./(1+c4.*T(c1,uVals))))./(1-N(c1,c2,uVals)./R(c1,c2,c3,c4,uVals));...
-      1-(c4.*T(c1,uVals))./(R(c1,c2,c3,c4,uVals)-N(c1,c2,uVals))];%1-(1-(1./(1+c4.*T(c1,uVals))))./(1-N(c1,c2,uVals)./R(c1,c2,c3,c4,uVals))];
+  relativeOpeningH = [d_open./h(c1,c2,c3,c4,uVals); 1-d_open./h(c1,c2,c3,c4,uVals)];
+%   relativeOpeningH = [c4.*T(c1,uVals)./(R(c1,c2,c3,c4,uVals)-N(c1,c2,uVals));...(1-(1./(1+c4.*T(c1,uVals))))./(1-N(c1,c2,uVals)./R(c1,c2,c3,c4,uVals));...
+%       1-(c4.*T(c1,uVals))./(R(c1,c2,c3,c4,uVals)-N(c1,c2,uVals))];%1-(1-(1./(1+c4.*T(c1,uVals))))./(1-N(c1,c2,uVals)./R(c1,c2,c3,c4,uVals))];
   bar(uVals,relativeOpeningH',1.5,'Stacked')
   title('Relative contribution to histone loss','FontSize',fontSize)
   xlabel(ax7,'UV dose','FontSize',fontSize); 

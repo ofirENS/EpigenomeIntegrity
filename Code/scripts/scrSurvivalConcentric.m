879a1c4372ca78ce3195f3806dfe07fd97ae60f6
ofirEns <ofirshukron@gmail.com>
2015-12-08 16:31:22 +0100

    started working on histone sliding distance

--- Code/scripts/scrSurvivalConcentric.m
+++ Code/scripts/scrSurvivalConcentric.m
@@ -1,12 +1,12 @@
 % calculate hazard and survival for signal transfered between concentric rings 
-R     = 3; % [mu/m]
-dr    = 0.01;
-startRadius = 0.01;
+R     = 3.5; % [mu/m]
+dr    = 0.005;
+startRadius = 1e-10;
 r     = startRadius:dr:R;
 f     = @(r)r;
 y     = @(r) (0.15*r.^2) -0.0087.*r -0.64;
 intFR = @(r) (0.15/4).*r.^4 -(0.0087/3).*r.^3 -(0.64/2).*r.^2;
-h     = @(r)  intFR(r)./(f(r)-intFR(r))+1;% the hazard function 
+h     = @(r)  f(r)./(f(r)-intFR(r));% the hazard function 
 s     = zeros(1,numel(r));
 tr    = s;
 s(1)  = 1;
@@ -15,11 +15,11 @@ for rIdx = 2:numel(r)
     s(rIdx)  = exp(-trapz(r(1:rIdx),h(r(1:rIdx))));
     tr(rIdx) = trapz(r(1:rIdx),r(1:rIdx).*s(1:rIdx));
 end
-
+tr = tr./trapz(r,tr);
 H   = -log(s);
 F   = 1-s; % cdf
 f   = (1-F).*h(r);
-fig = figure;
+fig = figure('Units','norm');
 ax  = axes('Parent',fig);
 line('XData',r-r(1),'YData',s,'Color','b','Parent',ax,'DisplayName','survival');
 line('XData',r-r(1),'Ydata',h(r),'Color','r','Parent',ax,'DisplayName','hazard');
@@ -28,4 +28,8 @@ line('XData',r-r(1),'YData',F,'Color','k','Parent',ax,'DisplayName','cdf');
 line('XData',r-r(1),'YData',f,'Color','m','Parent',ax,'DisplayName','pdf');
 % line('XData',r,'YData',r,'Color','y','Parent',ax,'DisplayName','line');
 daspect(ax,[1 1 1]);
+xlabel(ax,'radius [\mum]')
 legend(ax,get(ax,'Children'))
+set(ax,'FontSize',25)
+set(get(ax,'Children'),'LineWidth',3)
+drawnow
